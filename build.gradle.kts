c2df7ff12aabbed42e0f01d94d98a5f5edb52d92
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2022-12-02 16:02:11 +0900

    PDF作成中

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     // https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
     implementation("org.xerial:sqlite-jdbc:3.40.0.0")
 
+    // https://mvnrepository.com/artifact/org.apache.pdfbox/pdfbox
+    implementation("org.apache.pdfbox:pdfbox:2.0.27")
+
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/jp/complexalpha/Main.java
+++ src/main/java/jp/complexalpha/Main.java
@@ -3,13 +3,27 @@ package jp.complexalpha;
 import jp.complexalpha.db.AddressInfo;
 import jp.complexalpha.db.Select;
 
+import java.io.File;
 import java.sql.Array;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String now = LocalDateTime.now().format(format);
+        System.out.println(now);
+
+        String outDir = System.getenv("PDF_OUT_DIR");
+        File outFile = new File(outDir + "/out_" + now + ".pdf");
+
+        System.out.println(outFile);
+
+        String fontPath = System.getenv("PDF_FONT");
+        System.out.println(fontPath);
 
         ArrayList<AddressInfo> list = Select.getInfo();
 
