2b6ae12a443ed5e637de0645b0f3dc35dc8387a8
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2022-12-03 08:11:27 +0900

    PDFファイル作成

--- src/main/java/jp/complexalpha/Main.java
+++ src/main/java/jp/complexalpha/Main.java
@@ -2,6 +2,13 @@ package jp.complexalpha;
 
 import jp.complexalpha.db.AddressInfo;
 import jp.complexalpha.db.Select;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.util.Matrix;
 
 import java.io.File;
 import java.sql.Array;
@@ -11,7 +18,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
 
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         String now = LocalDateTime.now().format(format);
@@ -28,5 +35,30 @@ public class Main {
         ArrayList<AddressInfo> list = Select.getInfo();
 
         list.forEach(s -> System.out.println(s.name));
+
+        try (PDDocument document = new PDDocument()) {
+
+            PDFont font = PDType0Font.load(document, new File(fontPath));
+
+            PDPage page = new PDPage(PDRectangle.A4);
+            page.setRotation(90);
+            document.addPage(page);
+
+
+            try (PDPageContentStream content = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, false)) {
+                content.transform(new Matrix(0, 1, -1, 0, page.getMediaBox().getWidth(), 0));
+            }
+
+            try (PDPageContentStream content = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, false)) {
+
+                content.beginText();
+                content.setFont(font, 50);
+                content.newLineAtOffset(100, 200);
+                content.showText("こんにちは。世界");
+                content.endText();
+            }
+
+            document.save(outFile);
+        }
     }
 }
